b6a8b5f074fe2cf3d47f82ad066c634b90f3e212
gruve-p <jackielove4u@hotmail.com>
2024-01-18 20:33:06 +0100

    Fixes after merging upstream

--- contrib/build-wine/build-electrum-git.sh
+++ contrib/build-wine/build-electrum-git.sh
@@ -12,7 +12,7 @@ set -e
 
 pushd $WINEPREFIX/drive_c/electrum-grs
 
-VERSION=4.5.0
+VERSION=4.5.1
 info "Last commit: $VERSION"
 
 # Load electrum-locale for this release
--- electrum_grs/plugins/swapserver/__init__.py
+++ electrum_grs/plugins/swapserver/__init__.py
@@ -7,7 +7,7 @@ Submarine swap server for an Electrum-GRS daemon.
 Example setup:
 
   electrum-grs -o setconfig use_swapserver True
-  electrum-grs -o setconfig swapserver_address localhost:5455
+  electrum-grs -o setconfig swapserver_port 5455
   electrum-grs daemon -v
 
 """
